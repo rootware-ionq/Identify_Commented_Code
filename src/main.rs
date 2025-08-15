f06eb6b0aea5a888c1950801b181659557b15d93
Shah Saad Alam <shahsaad.alam@ionq.co>
2025-08-15 01:41:21 -0600

    Commit tool + scratch files

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{env, fs, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        eprintln!("Usage: {} <file.ris>", args[0]);
+        eprintln!("Usage: {} <file.py>", args[0]);
         process::exit(1);
     }
     let filepath = &args[1];
@@ -16,9 +16,33 @@ fn main() {
     let rules = comment_parser::get_syntax("python").unwrap();
     let parser = CommentParser::new(&python_code, rules);
 
+    println!(" ##############################");
+    println!(" Line # \t Extracted Comments:");
+    println!(" ##############################");
     for comment in parser {
-        if matches!(comment.clone(), LineComment) {
-            println!("{:#?}", comment);
+        match comment {
+            comment_parser::Event::LineComment(span, text) => {
+                if is_probable_code(&span) && !text.contains("# type: ignore") {
+                    println!(
+                        "{:#?}\t {:#?}",
+                        python_code[..python_code.find(span).unwrap()]
+                            .lines()
+                            .count(),
+                        span
+                    );
+                }
+            }
+
+            _ => {}
         }
     }
+}
+/// Heuristic function to detect code in a comment
+fn is_probable_code(s: &str) -> bool {
+    // Looks for Python keywords, operators, or function calls
+    let keywords = [
+        "def ", "return", "import ", "for ", "while ", "if ", "=", "print(", "(",
+    ];
+    let wrong_words = ["the"];
+    keywords.iter().any(|kw| s.contains(kw)) && !wrong_words.iter().any(|w| s.contains(w))
 }
